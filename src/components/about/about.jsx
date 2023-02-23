812560c1fa9d4a651744e95ea9372d9e825c885d
Dipak Kumar Paul <pauldipakkr@gmail.com>
2023-02-23 12:03:48 +0530

    new feb commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -39,9 +39,9 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon'/>
               <h2>Experience</h2>
-              <h3> <a className='exp-ancor' href="https://www.mindbrewstudio.com/"> Mind Brew Studio </a> - Currently Working</h3>
+              <h3> <a className='exp-ancor' href="https://www.mindbrewstudio.com/" target="_blank"> Mind Brew Studio </a> - Currently Working</h3>
               <h4>Web Developer</h4>
-              <h3> <a className='exp-ancor' href="http://www.stmarysgroupofinstitutions.in/"> St.Mary's Arcadian School </a> - Formar</h3>
+              <h3> <a className='exp-ancor' href="http://www.stmarysgroupofinstitutions.in/" target="_blank"> St.Mary's Arcadian School </a> - Formar</h3>
               <h4>Assistant Teacher</h4>
             </article>
           </div>
